ba882c0ce6583be88b861b8b7151f5832e382923
mely-0 <3145756813mela@gmail.com>
2023-01-17 17:40:11 -0500

    se le realizaron cambios en el archivo list-edit-router

--- list-edit-router.js
+++ list-edit-router.js
@@ -1,6 +1,18 @@
 const express = require("express");
 const router = express.Router();
 const fs = require("fs")
+
+router.use(express.json())
+router.get("/listar" ,express.json(), function(req, res){
+    let datos ;
+    fs.readFile('tareas.json', function(err, data) {
+        let tarea = data.toString();
+        datos = JSON.parse(tarea);
+        res.send({tareas: datos})
+    
+    })
+    
+    })
 router.post("/agregar", express.json(), function(req,res){
     const datas= req.body;
         let tareas;
@@ -41,15 +53,34 @@ router.delete("/eliminar/&:ide", (req, res)=>{
 })
 
 })
-router.put("/actualizado", express.json(), function(req, res){
+router.put("/actualizado/&:ide",(req, res)=>{
+    const ide= parseInt(req.params.ide);
     let datos ;
-    fs.readFile('tareas.json', function(err, data) {
-        let tarea = data.toString();
-        datos = JSON.parse(tarea);
-        res.send({"tareas actuales": datos})
+    const {body}= req;
+  
+    fs.readFile('tareas.json', (req, data)=>{
+        let datas= data.toString();
+        datos = JSON.parse(datas);
+
+        let tarea = datos.filter(task => task.id === ide )
+        tarea[0].tarea = body.tarea;
+        tarea[0].completada = body.completada;
+        tarea[0].descripcion = body.descripcion;
+        
+
+        let neww = datos.filter(task => task.id !== ide)
+        neww.push(tarea[0])
+
+        fs.unlink('tareas.json',(error)=>{
+            if(error) throw error
+        })  
+        fs.appendFile('tareas.json', JSON.stringify(neww), (error)=>{
+            if(error)throw error
+        })
+        
     
     })
-    
-    })
+})
+
 
 module.exports= router;
